ba8ce800d11e86efa5214ba3bdcb9ec7a209c78e
marcusvd <marcusmvd@gmail.com>
2025-02-14 11:47:56 -0300

    oneMore

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration;
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        CreateHostBuilder(args).Build().Run();
+    }
+
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args).UseWindowsService() //Configure the app to be executed as a Windows Service
+
+        .ConfigureAppConfiguration((hostContext, config) =>
+        {
+            //load settings from appsettings.json
+            config.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+        })
+        .ConfigureServices((hostContext, services) =>
+        {
+            services.AddHostedService<Worker>();
+        });
+
+}
